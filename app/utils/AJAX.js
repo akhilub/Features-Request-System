ac7e3cc1cfd93f045ec65e31875eb5111a38d482
Akhil Singh Chauhan <akhilsin@buffalo.edu>
2024-05-01 16:34:57 -0400

    updated Ajax for prod deployment

--- app/utils/AJAX.js
+++ app/utils/AJAX.js
@@ -1,5 +1,5 @@
-const Domain = 'http://127.0.0.1:8009';
-// const Domain = 'http://backend:8009';
+// const Domain = 'http://127.0.0.1:8009';
+const Domain = 'http://backend:8009';
 
 const SSOToken =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZW1haWwuY29tIiwiaWQiOiJ1c2VyLmlkIiwibmF5bWUiOiJ1c2VyLm5hbWUiLCJpYXQiOjE2MDk5NzMyMzl9.7VSmwQSmkhkEAba8zgd1_05b8hQKovTZtVHxFKa3MNk';
